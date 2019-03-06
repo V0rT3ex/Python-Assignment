fafd33e557f49f5dc6dcf330afa1c422d02f2081
Amos <amosbi12@gmail.com>
2019-03-06 17:50:50 +0200

    Will add check_cols function

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -15,3 +15,18 @@ def check_row(row):
         return element
 
 
+def check_cols(matrix):
+    """
+    This function receives the matrix and returns whether
+    there is a sequence of the same number in a col of the matrix.
+    If it does, it returns the number, otherwise it returns false.
+    """
+
+    # Initializing the rows of the matrix
+    row1, row2, row3 = matrix[0], matrix[1], matrix[2]
+
+    # Looping through each row
+    for i in range(3):
+        if row1[i] == row2[i] and row2[i] == row3[i]:
+            return row1[i]
+    return False
