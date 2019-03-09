21b1fdb65397dc6e50a85b30e3131a308488e54c
Amos <amosbi12@gmail.com>
2019-03-09 14:11:01 +0200

    Will add map_function function

---
+++ map_func.py
@@ -0,0 +1,10 @@
+def map_function(l, function):
+    """
+    This function receives a list - denoted by l,
+    and a function as parameters and returns the list after
+    each item of it has been modified by the function parameter.
+    """
+
+    l = [function(item) for item in l]
+    return l
+
